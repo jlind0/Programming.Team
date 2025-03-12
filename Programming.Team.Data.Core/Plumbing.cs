9fde1197712f36d601e0d526faf93930e1e67841
Luke Yoo <w.lukeyoo@gmail.com>
2025-03-11 22:22:55 -0700

    Reformat into the traditional style

--- Programming.Team.Data.Core/Plumbing.cs
+++ Programming.Team.Data.Core/Plumbing.cs
@@ -11,11 +11,13 @@ using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using System.Windows.Input;
 
-namespace Programming.Team.Data.Core {
+namespace Programming.Team.Data.Core
+{
     /// <summary>
     /// Create DbContext instances and manage user impersonation.
     /// </summary>
-    public interface IContextFactory {
+    public interface IContextFactory
+    {
         DbContext CreateContext();
         Task<ClaimsPrincipal?> GetPrincipal();
         Task<Guid?> GetImpersonatedUser();
@@ -24,20 +26,24 @@ namespace Programming.Team.Data.Core {
     /// <summary>
     /// Extract user ID.
     /// </summary>
-    public static class ClaimsPrincipalExtentension {
-        public static string? GetUserId(this ClaimsPrincipal? principal) {
+    public static class ClaimsPrincipalExtentension
+    {
+        public static string? GetUserId(this ClaimsPrincipal? principal)
+        {
             return principal?.Claims.FirstOrDefault(p => p.Type == "http://schemas.microsoft.com/identity/claims/objectidentifier")?.Value;
         }
     }
     /// <summary>
     /// Manage transactions and database context.
     /// </summary>
-    public interface IUnitOfWork : IAsyncDisposable {
+    public interface IUnitOfWork : IAsyncDisposable
+    {
         DbContext Context { get; }
         Task Rollback();
         Task Commit(CancellationToken token = default);
     }
-    public interface IRepository {
+    public interface IRepository
+    {
         IUnitOfWork CreateUnitOfWork();
         Task<Guid?> GetCurrentUserId(IUnitOfWork? uow = null, bool fetchTrueUserId = false, CancellationToken token = default);
     }
@@ -46,7 +52,8 @@ namespace Programming.Team.Data.Core {
     /// </summary>
     public interface IIRepository<in TEntity, TKey> : IRepository
         where TEntity : Entity<TKey>, new()
-        where TKey : struct {
+        where TKey : struct
+    {
         Task Delete(TKey id, IUnitOfWork? work = null, CancellationToken token = default);
         Task Delete(TEntity entity, IUnitOfWork? work = null, CancellationToken token = default);
         Task Add(TEntity entity, IUnitOfWork? work = null, CancellationToken token = default);
@@ -56,7 +63,8 @@ namespace Programming.Team.Data.Core {
     /// </summary>
     public interface IRepository<TEntity, TKey> : IIRepository<TEntity, TKey>
         where TEntity : Entity<TKey>, new()
-        where TKey : struct {
+        where TKey : struct
+    {
         Task<TEntity> Update(TEntity entity,
             IUnitOfWork? work = null,
             Func<IQueryable<TEntity>, IQueryable<TEntity>>? properites = null, CancellationToken token = default);
