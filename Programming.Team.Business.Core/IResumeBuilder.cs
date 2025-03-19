b08fd5783947d4408f107b654e52f02a68223c1e
Jason L. Lind <lind@yahoo.com>
2025-03-18 20:42:32 -0500

    business layer documentation

--- Programming.Team.Business.Core/IResumeBuilder.cs
+++ Programming.Team.Business.Core/IResumeBuilder.cs
@@ -7,16 +7,71 @@ using System.Threading.Tasks;
 
 namespace Programming.Team.Business.Core
 {
+    /// <summary>
+    /// Builds resumes and postings
+    /// </summary>
     public interface IResumeBuilder
     {
+        /// <summary>
+        /// Builds a resume for a user
+        /// </summary>
+        /// <param name="userId">Target User.Id</param>
+        /// <param name="progress">Optional progress indicator</param>
+        /// <param name="token">Cancellation token</param>
+        /// <returns>A Resume</returns>
         Task<Resume> BuildResume(Guid userId, IProgress<string>? progress = null, CancellationToken token = default);
+        /// <summary>
+        /// Builds a posting for a user
+        /// </summary>
+        /// <param name="userId">Target User.Id</param>
+        /// <param name="documentTemplateId">Target DocumentTempate.Id</param>
+        /// <param name="name">Name of position</param>
+        /// <param name="positionText">Job Description</param>
+        /// <param name="resume">A Built Resume object</param>
+        /// <param name="progress">Optional progress indicator</param>
+        /// <param name="config">Optional configuration for the build</param>
+        /// <param name="token">Cancellation Token</param>
+        /// <returns>a Job Posting</returns>
         Task<Posting> BuildPosting(Guid userId, Guid documentTemplateId, string name, string positionText, Resume resume, IProgress<string>? progress = null, ResumeConfiguration? config = null, CancellationToken token = default);
+        /// <summary>
+        /// Rebuilds a posting with a new resume
+        /// </summary>
+        /// <param name="posting">The existing posting</param>
+        /// <param name="resume">The target Resume</param>
+        /// <param name="enrich">Indicates AI enhancements to be used (will NOT count a resume generation)</param>
+        /// <param name="renderPDF">Indicates if the PDF should be rendered</param>
+        /// <param name="progress">An optional progress indicator</param>
+        /// <param name="config">An optional configuration</param>
+        /// <param name="token">An optional cancellation token</param>
+        /// <returns>The rebuilt Job Posting</returns>
         Task<Posting> RebuildPosting(Posting posting, Resume resume, bool enrich = true, bool renderPDF = true, IProgress<string>? progress = null, ResumeConfiguration? config = null, CancellationToken token = default);
+        /// <summary>
+        /// Renders a resume to a PDF
+        /// </summary>
+        /// <param name="posting">The target posting</param>
+        /// <param name="token">optional cancellation token</param>
+        /// <returns>The work of rendering a resume</returns>
         Task RenderResume(Posting posting, CancellationToken token = default);
     }
+    /// <summary>
+    /// Facade to working with storage for Resumes
+    /// </summary>
     public interface IResumeBlob
     {
+        /// <summary>
+        /// Uploads a resume to a posting
+        /// </summary>
+        /// <param name="postingId">Posting.Id</param>
+        /// <param name="pdfData">PDF Bytes</param>
+        /// <param name="token">Optional Cancellation Token</param>
+        /// <returns>A Task representing the work</returns>
         Task UploadResume(Guid postingId, byte[] pdfData, CancellationToken token = default);
+        /// <summary>
+        /// Gets a resume for a posting
+        /// </summary>
+        /// <param name="postingId">Posting.Id</param>
+        /// <param name="token">Cancellation Token</param>
+        /// <returns>bytes for a resume</returns>
         Task<byte[]?> GetResume(Guid postingId, CancellationToken token = default);
     }
 }
--- Programming.Team.Business.Core/IUserBusinessFacade.cs
+++ Programming.Team.Business.Core/IUserBusinessFacade.cs
@@ -10,25 +10,105 @@ using System.Threading.Tasks;
 
 namespace Programming.Team.Business.Core
 {
+    /// <summary>
+    /// User Business Facade
+    /// </summary>
     public interface IUserBusinessFacade : IBusinessRepositoryFacade<User, Guid>
     {
+        /// <summary>
+        /// Get user by Azure AD B2C object id
+        /// </summary>
+        /// <param name="objectId">Target Azure AD B2C Object Id</param>
+        /// <param name="work">Optional unit of work</param>
+        /// <param name="properties">Optional User Properites to load</param>
+        /// <param name="token">Optional cancellation token</param>
+        /// <returns>User, if found</returns>
         Task<User?> GetByObjectIdAsync(string objectId, IUnitOfWork? work = null, Expression<Func<User, object>>? properties = null, CancellationToken token = default);
+        /// <summary>
+        /// Add a recruiter to a user
+        /// </summary>
+        /// <param name="targetUserId">Target User.Id</param>
+        /// <param name="recruiterId">Recruiter's User.Id</param>
+        /// <param name="work">Optional Unit of Work</param>
+        /// <param name="token">Optional Cancellation Token</param>
+        /// <returns>Task representing work of adding a recruiter</returns>
         Task AddRecruiter(Guid targetUserId, Guid recruiterId, IUnitOfWork? work = null, CancellationToken token = default);
+        /// <summary>
+        /// Remove a recruiter from a user
+        /// </summary>
+        /// <param name="targetUserId">Target User.Id</param>
+        /// <param name="recruiterId">Recruiter User.Id</param>
+        /// <param name="work">Optional Unit of Work</param>
+        /// <param name="token">Optional Cancellation Token</param>
+        /// <returns>Task representing work of removing a recruiter</returns>
         Task RemoveRecruiter(Guid targetUserId, Guid recruiterId, IUnitOfWork? work = null, CancellationToken token = default);
+        /// <summary>
+        /// Utilize a resume generation, will decrement User.ResumeGenerationsLeft if user is not an Admin
+        /// </summary>
+        /// <param name="userId">Target User.Id</param>
+        /// <param name="work">optional Unit of Work</param>
+        /// <param name="token">Optional Cancellation Token</param>
+        /// <returns>Boolean indicating if the resume generation can be executed</returns>
         Task<bool> UtilizeResumeGeneration(Guid userId, IUnitOfWork? work = null, CancellationToken token = default);
     }
+    /// <summary>
+    /// Role Business Facade
+    /// </summary>
     public interface IRoleBusinessFacade : IBusinessRepositoryFacade<Role, Guid>
     {
+        /// <summary>
+        /// Get User Ids for a Role
+        /// </summary>
+        /// <param name="roleId">Target Role.Id</param>
+        /// <param name="work">Optional Unit of Work</param>
+        /// <param name="token">Optional Cancellation Token</param>
+        /// <returns>Array of User.Id's</returns>
         Task<Guid[]> GetUserIds(Guid roleId, IUnitOfWork? work = null, CancellationToken token = default);
+        /// <summary>
+        /// Set selected users to a role
+        /// </summary>
+        /// <param name="roleId">Target Role.Id</param>
+        /// <param name="userIds">Target User.Id's</param>
+        /// <param name="work">Optional Unit of Work</param>
+        /// <param name="token">Optional Cancellation Token</param>
+        /// <returns>Task represent work of assigning roles</returns>
         Task SetSelectedUsersToRole(Guid roleId, Guid[] userIds, IUnitOfWork? work = null, CancellationToken token = default);
     }
+    /// <summary>
+    /// Section Template Business Facade
+    /// </summary>
     public interface ISectionTemplateBusinessFacade : IBusinessRepositoryFacade<SectionTemplate, Guid>
     {
+        /// <summary>
+        /// Get Section Templates by Section
+        /// </summary>
+        /// <param name="sectionId">Target SectionTemplate.SectionId</param>
+        /// <param name="work">Optional Unit of Work</param>
+        /// <param name="token">Optional Cancellation Token</param>
+        /// <returns></returns>
         Task<SectionTemplate[]> GetBySection(ResumePart sectionId, IUnitOfWork? work = null, CancellationToken token = default);
+        /// <summary>
+        /// Get the default section template
+        /// </summary>
+        /// <param name="sectionId">Target SectionTemplate.SectionId</param>
+        /// <param name="work">Optional Unit of Work</param>
+        /// <param name="defaultName">Optional Override "Default" section name</param>
+        /// <param name="token">Optional Cancellation Token</param>
+        /// <returns>Section Template, if found</returns>
         Task<SectionTemplate?> GetDefaultSection(ResumePart sectionId, IUnitOfWork? work = null, string defaultName = "Default", CancellationToken token = default);
     }
+    /// <summary>
+    /// Skills Business Facade
+    /// </summary>
     public interface ISkillsBusinessFacade : IBusinessRepositoryFacade<Skill, Guid>
     {
+        /// <summary>
+        /// Get Skills excluding those already assigned to a given Position
+        /// </summary>
+        /// <param name="positionId">Target Position.Id</param>
+        /// <param name="work">Optional Unit of Work</param>
+        /// <param name="token">Optional Cancellation Token</param>
+        /// <returns>Array of Skills not assigned to the given position but exist for that user</returns>
         Task<Skill[]> GetSkillsExcludingPosition(Guid positionId, IUnitOfWork? work = null,
             CancellationToken token = default);
     }
