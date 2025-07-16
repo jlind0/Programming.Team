a307e0a3ea58a256a0af5eeaa725b67139943677
spinningCat <beratn@gmail.com>
2025-07-16 22:03:10 +0300

    some changes sun popover and validation

--- Programming.Team.ViewModels/Resume/EducationViewModels.cs
+++ Programming.Team.ViewModels/Resume/EducationViewModels.cs
@@ -6,6 +6,7 @@ using Programming.Team.Core;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.Design;
 using System.Linq;
 using System.Linq.Expressions;
@@ -22,6 +23,9 @@ namespace Programming.Team.ViewModels.Resume
         }
 
         private string name = null!;
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, ErrorMessage = "Name can't be longer than 100 characters.")]
         public string Name
         {
             get => name;
